979376dbe6c502396c1f12d0e5dcb08e39684afb
Abha Gupta <agupta3@walmart.com>
2020-05-16 23:50:49 -0700

    first commit

---
+++ src/queries/queries.js
@@ -0,0 +1,54 @@
+import {gql} from "apollo-boost";
+
+const  getBooksQuery = gql`
+  {
+      books {
+          name
+          id
+      }
+  }
+`
+
+const  getAuthorsQuery = gql`
+  {
+      authors {
+          name
+          id
+      }
+  }`
+  const getBookQuery = gql`
+    query($id: String) {
+        book(id: $id) {
+            id
+            name
+            genre
+            author {
+                id
+                name
+                books{
+                    name
+                    id
+                }
+            }
+        }
+    }
+  `
+
+  const addBookMutation = gql`
+
+    mutation ($name: String!, $genre: String!, $authorId: String!){
+        addBook(name: $name, genre:$genre, authorId:$authorId) {
+            name
+            id
+        }
+    }
+    `
+
+  
+
+  export {
+      getAuthorsQuery,
+      getBooksQuery,
+      getBookQuery,
+      addBookMutation
+  }
